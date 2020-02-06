b01143aeb4f709229003d09cae6cee4ac57c2d19
Mahaman Sani SAHIROU ADAMOU <mahaman.sahirou@gmail.com>
2020-02-07 00:42:48 +0100

    Final Commit  2020-02-06

--- app/src/main/java/com/example/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/miwok/NumbersActivity.java
@@ -1,6 +1,7 @@
 package com.example.miwok;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,22 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Add an array of words
+        String[] words = new String[10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
+        for (int i = 0; i < 10; i++) {
+            Log.v("NumberActivity: ", "Word at position " + i + ": " + words[i]);
+        }
     }
 }

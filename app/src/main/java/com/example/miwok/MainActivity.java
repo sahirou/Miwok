8cb9e479b7172e04d5630d5100eac7533e7837cb
Mahaman Sani SAHIROU ADAMOU <mahaman.sahirou@gmail.com>
2020-02-06 15:24:54 +0100

    Add Onclick

--- app/src/main/java/com/example/miwok/MainActivity.java
+++ app/src/main/java/com/example/miwok/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.miwok;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addNumbersList(View view) {
+        Intent numbers = new Intent(this, NumbersActivity.class);
+        startActivity(numbers);
+    }
+
+    public void addColorsList(View view) {
+        Intent colors = new Intent(this, ColorsActivity.class);
+        startActivity(colors);
+    }
+
+    public void addPhrasesList(View view) {
+        Intent phrases = new Intent(this, PhrasesActivity.class);
+        startActivity(phrases);
+    }
+
+    public void addFamilyList(View view) {
+        Intent family = new Intent(this, FamilyActivity.class);
+        startActivity(family);
     }
 }
